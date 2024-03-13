449bed88b0b248b10cd06d38087b1057451695e1
Gabriel Amorim <mrgconta@gmail.com>
2024-03-13 17:38:03 -0300

    Ajustes iniciais

--- back/UTalk/obj/Debug/net8.0/UTalk.AssemblyInfo.cs
+++ back/UTalk/obj/Debug/net8.0/UTalk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UTalk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d07c6425c874542a509989ba896a0fd13dd01a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UTalk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UTalk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
